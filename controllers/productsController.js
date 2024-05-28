a5ded08c8c14ec2ee086dfc0db0ab7ce5f3580f4
khoa <user.iamjasper@gmail.com>
2024-05-28 18:18:19 +0700

    add product api

---
+++ controllers/productsController.js
@@ -0,0 +1,55 @@
+const Product = require('../models/Product')
+
+const productsController = {
+    createProduct: async (req, res) => {
+        const newProduct = new Product(req.body)
+        try {
+            await newProduct.save()
+            res.status(200).json('product created')
+        } catch (error) {
+            res.status(500).json('failed to create product')
+        }
+    },
+
+    getAllProducts: async (req, res) => {
+        try {
+            const products = await Product.find().sort({ createdAt: -1 })
+            res.status(200).json(products)
+        } catch (error) {
+            res.status(500).json('failed to get the products')
+        }
+    },
+
+    getProduct: async (req, res) => {
+        const productId = req.params.id
+        try {
+            const product = await Product.findById(productId)
+            const { __v, createdAt, ...productData } = product._doc
+            res.status(200).json(productData)
+        } catch (error) {
+            res.status(500).json('failed to get the product')
+        }
+    },
+
+    searchProducts: async (req, res) => {
+        try {
+            const results = await Product.aggregate([
+                {
+                    $search: {
+                        index: 'default',
+                        text: {
+                            query: req.params.key,
+                            path: {
+                                wildcard: '*',
+                            },
+                        },
+                    },
+                },
+            ])
+            res.status(200).json(results)
+        } catch (error) {
+            res.status(500).json('failed to get the product')
+        }
+    },
+}
+module.exports = productsController
--- index.js
+++ index.js
@@ -2,7 +2,8 @@ const express = require('express')
 const app = express()
 const dotenv = require('dotenv')
 const mongoose = require('mongoose')
-const port = 3000
+const productRoute = require('./routes/products')
+const port = 8080
 
 dotenv.config()
 mongoose
@@ -10,5 +11,7 @@ mongoose
     .then(() => console.log('db connected'))
     .catch((err) => console.log(err))
 
-app.get('/', (req, res) => res.send('Hello World!'))
-app.listen(process.env.PORT || port, () => console.log(`Example app listening on port ${port}!`))
+app.use(express.json({ limit: '10mb' }))
+app.use(express.urlencoded({ limit: '10mb', extended: true }))
+app.use('/api/products', productRoute)
+app.listen(process.env.PORT || port, () => console.log(`Example app listening on port ${process.env.PORT}!`))
