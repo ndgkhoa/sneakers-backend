871ec1466fa558e835453e77dd1de5dec17ac454
khoa <user.iamjasper@gmail.com>
2024-05-29 23:44:45 +0700

    add authController

---
+++ controllers/authController.js
@@ -0,0 +1,48 @@
+const User = require('../models/User')
+
+const CryptoJS = require('crypto-js')
+const jwt = require('jsonwebtoken')
+
+const authController = {
+    createUser: async (req, res) => {
+        const newUser = new User({
+            username: req.body.username,
+            email: req.body.email,
+            password: CryptoJS.AES.encrypt(req.body.password, process.env.SECRET_KEY).toString(),
+            location: req.body.location,
+        })
+        try {
+            await newUser.save()
+            res.status(201).json({ message: 'User successfully created' })
+        } catch (error) {
+            res.status(500).json({ message: error })
+        }
+    },
+
+    loginUser: async (req, res) => {
+        try {
+            const user = await User.findOne({ email: req.body.email })
+            !user && res.status(401).json('Could not find the user')
+
+            const decryptedpass = CryptoJS.AES.decrypt(user.password, process.env.SECRET_KEY)
+            const thepassword = decryptedpass.toString(CryptoJS.enc.Utf8)
+
+            thepassword !== req.body.password && res.status(401).json('Wrong Password')
+
+            const userToken = jwt.sign(
+                {
+                    id: user._id,
+                },
+                process.env.JWT_PRIVATE_KEY,
+                { expiresIn: '1h' },
+            )
+
+            const { password, __v, updatedAt, createdAt, ...others } = user._doc
+
+            res.status(200).json({ ...others, token: userToken })
+        } catch (error) {
+            res.status(500).json('Failed to login, check your credentials')
+        }
+    },
+}
+module.exports = authController
--- index.js
+++ index.js
@@ -3,6 +3,7 @@ const app = express()
 const dotenv = require('dotenv')
 const mongoose = require('mongoose')
 const productRoute = require('./routes/products')
+const authRoute = require('./routes/auth')
 const port = 8080
 
 dotenv.config()
@@ -13,5 +14,8 @@ mongoose
 
 app.use(express.json({ limit: '10mb' }))
 app.use(express.urlencoded({ limit: '10mb', extended: true }))
+
 app.use('/api/products', productRoute)
+app.use('/api/', authRoute)
+
 app.listen(process.env.PORT || port, () => console.log(`Example app listening on port ${process.env.PORT}!`))
